d25e7e5244f6c0efdbe4e3c55a244ecb5aa4c6e9
Sean Melody <seandmelody@gmail.com>
2020-10-24 17:21:53 -0700

    Added current date to title

--- Assets/script.js
+++ Assets/script.js
@@ -1,1 +1,5 @@
 console.log("Hi")
+
+var currentDay = document.querySelector("#currentDay")
+currentDay.textContent = moment().format('MMMM Do YYYY')
+// currentDay.textContent(moment().format('MMMM Do YYYY, h:mm:ss a'));
